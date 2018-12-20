f3140bd5655f43e3d26f9f3f3ea16b8f2ec940cf
pcrubianoa <pcrubianoa@gmail.com>
2018-12-20 17:36:47 -0500

    page containers

--- index.php
+++ index.php
@@ -20,63 +20,69 @@ if(isset($_SESSION['userLoggedIn'])){
     <title>Welcome to SpotifyClone</title>
 </head>
 <body>
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            <div id="nowPlayingLeft">
-                <div class="content">
-                    <div class="albumLink">
-                    <span class="trackName">
-                        <img src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg" alt="" class="albumArtwork"></div>
-                    </span>
-                    <div class="trackInfo">
+
+    <div id="mainContainer">
+        <div id="topContainer">
+            
+        </div>
+        <div id="nowPlayingBarContainer">
+            <div id="nowPlayingBar">
+                <div id="nowPlayingLeft">
+                    <div class="content">
+                        <div class="albumLink">
                         <span class="trackName">
-                            <span>The number of the beast</span>
+                            <img src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg" alt="" class="albumArtwork"></div>
                         </span>
-                        <span class="artistName">
-                            <span>Iron Maiden</span>
-                        </span>
-                    </div>
-                </div>
-            </div>
-            <div class="content playerControls">
-                <div class="buttons">
-                    <button class="controlButton shuffle" title="Shuffle button">
-                        <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                    </button>
-                    <button class="controlButton previous" title="Previous button">
-                        <img src="assets/images/icons/previous.png" alt="Shuffle">
-                    </button>
-                    <button class="controlButton play" title="Play button">
-                        <img src="assets/images/icons/play.png" alt="Shuffle">
-                    </button>
-                    <button class="controlButton pause" title="Pause button" style="display: none;">
-                        <img src="assets/images/icons/pause.png" alt="Pause">
-                    </button>
-                    <button class="controlButton next" title="Next button">
-                        <img src="assets/images/icons/next.png" alt="Shuffle">
-                    </button>
-                    <button class="controlButton repeat" title="Repeat button">
-                        <img src="assets/images/icons/repeat.png" alt="Shuffle">
-                    </button>
-                </div>
-                <div class="playbackBar">
-                    <span class="progressTime current">0.00</span>
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
+                        <div class="trackInfo">
+                            <span class="trackName">
+                                <span>The number of the beast</span>
+                            </span>
+                            <span class="artistName">
+                                <span>Iron Maiden</span>
+                            </span>
                         </div>
                     </div>
-                    <span class="progressTime remaining">0.00</span>
                 </div>
-            </div>
-            <div id="nowPlayingRight">
-                <div class="volumeBar">
-                    <button class="controlButton volume" title="Volume button">
-                        <img src="assets/images/icons/volume.png" alt="Volume">
-                    </button>
-                    <div class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton previous" title="Previous button">
+                            <img src="assets/images/icons/previous.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton play" title="Play button">
+                            <img src="assets/images/icons/play.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton pause" title="Pause button" style="display: none;">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+                        <button class="controlButton next" title="Next button">
+                            <img src="assets/images/icons/next.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton repeat" title="Repeat button">
+                            <img src="assets/images/icons/repeat.png" alt="Shuffle">
+                        </button>
+                    </div>
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
+                    </div>
+                </div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume" title="Volume button">
+                            <img src="assets/images/icons/volume.png" alt="Volume">
+                        </button>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
