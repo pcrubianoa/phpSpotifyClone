f945620c8b56a050be7414e2ebe221283b7d930f
pcrubianoa <pcrubianoa@gmail.com>
2018-12-20 18:48:13 -0500

    create albums, artists, genres, songs tables

--- database.sql
+++ database.sql
@@ -1,14 +1,83 @@
-CREATE TABLE `users` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`username` VARCHAR(25) NOT NULL DEFAULT '0',
-	`firstName` VARCHAR(50) NOT NULL DEFAULT '0',
-	`lastName` VARCHAR(50) NOT NULL DEFAULT '0',
-	`email` VARCHAR(200) NOT NULL DEFAULT '0',
-	`password` VARCHAR(32) NOT NULL DEFAULT '0',
-	`signUpDate` DATETIME NULL DEFAULT NULL,
-	`profilePic` VARCHAR(500) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`id`)
-)
-COLLATE='latin1_swedish_ci'
-ENGINE=InnoDB
-;
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               5.7.19 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for spotifyclone
+DROP DATABASE IF EXISTS `spotifyclone`;
+CREATE DATABASE IF NOT EXISTS `spotifyclone` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `spotifyclone`;
+
+-- Dumping structure for table spotifyclone.albums
+DROP TABLE IF EXISTS `albums`;
+CREATE TABLE IF NOT EXISTS `albums` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) NOT NULL DEFAULT '0',
+  `artist` int(11) NOT NULL DEFAULT '0',
+  `genre` int(11) NOT NULL DEFAULT '0',
+  `artworkPath` varchar(500) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table spotifyclone.artists
+DROP TABLE IF EXISTS `artists`;
+CREATE TABLE IF NOT EXISTS `artists` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table spotifyclone.genres
+DROP TABLE IF EXISTS `genres`;
+CREATE TABLE IF NOT EXISTS `genres` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table spotifyclone.songs
+DROP TABLE IF EXISTS `songs`;
+CREATE TABLE IF NOT EXISTS `songs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) NOT NULL DEFAULT '0',
+  `artist` int(11) NOT NULL DEFAULT '0',
+  `album` int(11) NOT NULL DEFAULT '0',
+  `genre` int(11) NOT NULL DEFAULT '0',
+  `duration` varchar(8) NOT NULL DEFAULT '0',
+  `path` varchar(5000) NOT NULL DEFAULT '0',
+  `albumOrder` int(11) NOT NULL DEFAULT '0',
+  `plays` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table spotifyclone.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL DEFAULT '0',
+  `firstName` varchar(50) NOT NULL DEFAULT '0',
+  `lastName` varchar(50) NOT NULL DEFAULT '0',
+  `email` varchar(200) NOT NULL DEFAULT '0',
+  `password` varchar(32) NOT NULL DEFAULT '0',
+  `signUpDate` datetime DEFAULT NULL,
+  `profilePic` varchar(500) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
