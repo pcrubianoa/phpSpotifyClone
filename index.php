36a936c8ebc69f42e5e0393d4ed807587eb2e5bb
pcrubianoa <pcrubianoa@gmail.com>
2018-12-20 16:39:59 -0500

    Volume bar

--- index.php
+++ index.php
@@ -62,12 +62,24 @@ if(isset($_SESSION['userLoggedIn'])){
                 <div class="playbackBar">
                     <span class="progressTime current">0.00</span>
                     <div class="progressBar">
-                        <div class="progressBarBg"></div>
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
                     </div>
                     <span class="progressTime remaining">0.00</span>
                 </div>
             </div>
             <div id="nowPlayingRight">
+                <div class="volumeBar">
+                    <button class="controlButton volume" title="Volume button">
+                        <img src="assets/images/icons/volume.png" alt="Volume">
+                    </button>
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
