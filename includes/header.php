89d9450705f3f8b33e75399e7516a091462ca266
pcrubianoa <pcrubianoa@gmail.com>
2018-12-20 21:00:08 -0500

    Header and footer files

---
+++ includes/header.php
@@ -0,0 +1,28 @@
+<?php
+include("includes/config.php");
+
+//session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])){
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+}else{
+    header("Location: register.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Welcome to SpotifyClone</title>
+</head>
+<body>
+
+    <div id="mainContainer">
+        <div id="topContainer">
+           <?php include("includes/navBarContainer.php"); ?>
+           <div id="mainViewContainer">
+               <div id="mainContent">
--- index.php
+++ index.php
@@ -1,36 +1,4 @@
-<?php
-include("includes/config.php");
+<?php  include("includes/header.php"); ?>
 
-//session_destroy();
+<?php  include("includes/footer.php"); ?>
 
-if(isset($_SESSION['userLoggedIn'])){
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-}else{
-    header("Location: register.php");
-}
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Welcome to SpotifyClone</title>
-</head>
-<body>
-
-    <div id="mainContainer">
-        <div id="topContainer">
-           <?php include("includes/navBarContainer.php"); ?>
-           <div id="mainViewContainer">
-               <div id="mainContent">
-
-               </div>
-           </div>
-        </div>
-        <?php include("includes/nowPlayingBar.php"); ?>
-    </div>
-</body>
-</html>
