012dc59a9587a52461d5b3801016adb8f988938c
pcrubianoa <pcrubianoa@gmail.com>
2018-12-20 20:32:45 -0500

    Organisational index

--- index.php
+++ index.php
@@ -23,95 +23,9 @@ if(isset($_SESSION['userLoggedIn'])){
 
     <div id="mainContainer">
         <div id="topContainer">
-            <div id="navBarContainer">
-                <nav class="navBar">
-                    <a href="index.php" class="logo">
-                        <img src="assets/images/icons/logo.png" alt="">
-                    </a>
-                    <div class="group">
-                        <div class="navItem">
-                            <a href="search.php" class="navItemLink">Search
-                            <img src="assets/images/icons/search.png" class="icon" alt="Search">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="group">
-                        <div class="navItem">
-                            <a href="browse.php" class="navItemLink">Browse</a>
-                        </div>
-                        <div class="navItem">
-                            <a href="yourMusic.php" class="navItemLink">Your music</a>
-                        </div>
-                        <div class="navItem">
-                            <a href="profile.php" class="navItemLink">Lastest</a>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+           <?php include("includes/navBarContainer.php"); ?>
         </div>
-        <div id="nowPlayingBarContainer">
-            <div id="nowPlayingBar">
-                <div id="nowPlayingLeft">
-                    <div class="content">
-                        <div class="albumLink">
-                        <span class="trackName">
-                            <img src="https://pbs.twimg.com/profile_images/905183271046193153/q_P1KBUJ_400x400.jpg" alt="" class="albumArtwork"></div>
-                        </span>
-                        <div class="trackInfo">
-                            <span class="trackName">
-                                <span>The number of the beast</span>
-                            </span>
-                            <span class="artistName">
-                                <span>Iron Maiden</span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div class="content playerControls">
-                    <div class="buttons">
-                        <button class="controlButton shuffle" title="Shuffle button">
-                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                        </button>
-                        <button class="controlButton previous" title="Previous button">
-                            <img src="assets/images/icons/previous.png" alt="Shuffle">
-                        </button>
-                        <button class="controlButton play" title="Play button">
-                            <img src="assets/images/icons/play.png" alt="Shuffle">
-                        </button>
-                        <button class="controlButton pause" title="Pause button" style="display: none;">
-                            <img src="assets/images/icons/pause.png" alt="Pause">
-                        </button>
-                        <button class="controlButton next" title="Next button">
-                            <img src="assets/images/icons/next.png" alt="Shuffle">
-                        </button>
-                        <button class="controlButton repeat" title="Repeat button">
-                            <img src="assets/images/icons/repeat.png" alt="Shuffle">
-                        </button>
-                    </div>
-                    <div class="playbackBar">
-                        <span class="progressTime current">0.00</span>
-                        <div class="progressBar">
-                            <div class="progressBarBg">
-                                <div class="progress"></div>
-                            </div>
-                        </div>
-                        <span class="progressTime remaining">0.00</span>
-                    </div>
-                </div>
-                <div id="nowPlayingRight">
-                    <div class="volumeBar">
-                        <button class="controlButton volume" title="Volume button">
-                            <img src="assets/images/icons/volume.png" alt="Volume">
-                        </button>
-                        <div class="progressBar">
-                            <div class="progressBarBg">
-                                <div class="progress"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include("includes/nowPlayingBar.php"); ?>
     </div>
 </body>
 </html>
