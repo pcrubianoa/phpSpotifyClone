7f4d3bba5fa1ad2321cd8acc5e0c43dedaff6148
pcrubianoa <pcrubianoa@gmail.com>
2018-12-24 17:48:18 -0500

    Styling search input

--- search.php
+++ search.php
@@ -11,5 +11,5 @@
 
 <div class="searchContainer">
     <h4>Search for an artist, album or song</h4>
-    <input type="text" class="searchInput" value="<'php echo $term; ?>'" placeholder="Start typeing...">
+    <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typeing...">
 </div>
