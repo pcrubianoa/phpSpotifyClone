514348e788083f06e63187fab0fbf253ed6261a2
pcrubianoa <pcrubianoa@gmail.com>
2018-12-26 13:46:22 -0500

    Show and Hide options menu

--- album.php
+++ album.php
@@ -58,3 +58,8 @@
 
     </ul>
 </div>
+
+<nav class="optionsMenu">
+	<input type="hidden" class="songId">
+	<?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername()); ?>
+</nav>
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,18 @@ var shuffle = false;
 var userLoggedIn;
 var timer;
 
+$(document).click(function(click) {
+	var target = $(click.target);
+
+	if(!target.hasClass("item") && !target.hasClass("optionsButton")) {
+		hideOptionsMenu();
+	}
+});
+
+$(window).scroll(function() {
+	hideOptionsMenu();
+});
+
 function openPage(url){
 
     if(timer != null){
@@ -57,6 +69,29 @@ function deletePlaylist(playlistId) {
 			openPage("yourMusic.php");
 		});
 	}
+}
+
+function hideOptionsMenu() {
+	var menu = $(".optionsMenu");
+	if(menu.css("display") != "none") {
+		menu.css("display", "none");
+	}
+}
+
+function showOptionsMenu(button) {
+	var songId = $(button).prevAll(".songId").val();
+	var menu = $(".optionsMenu");
+	var menuWidth = menu.width();
+	menu.find(".songId").val(songId);
+
+	var scrollTop = $(window).scrollTop(); //Distance from top of window to top of document
+	var elementOffset = $(button).offset().top; //Distance from top of document
+
+	var top = elementOffset - scrollTop;
+	var left = $(button).position().left;
+
+	menu.css({ "top": top + "px", "left": left - menuWidth + "px", "display": "inline" });
+
 }
 
 function formatTime(seconds) {
