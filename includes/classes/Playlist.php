61e885f8b0d99eb272db03d2367532239b4ec807
pcrubianoa <pcrubianoa@gmail.com>
2018-12-26 12:43:25 -0500

    Displaying and delete playlist

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -28,7 +28,31 @@
         
 		public function getOwner() {
 			return $this->owner;
-		}
+        }
+        
+		public function getNumberOfSongs() {
+			$query = mysqli_query($this->con, "SELECT songId FROM playlistSongs WHERE playlistId='$this->id'");
+			return mysqli_num_rows($query);
+        }
 
+		public function getSongIds() {
+			$query = mysqli_query($this->con, "SELECT songId FROM playlistSongs WHERE playlistId='$this->id' ORDER BY playlistOrder ASC");
+			$array = array();
+			while($row = mysqli_fetch_array($query)) {
+				array_push($array, $row['songId']);
+			}
+			return $array;
+		}
+		public static function getPlaylistsDropdown($con, $username) {
+			$dropdown = '<select class="item playlist">
+							<option value="">Add to playlist</option>';
+			$query = mysqli_query($con, "SELECT id, name FROM playlists WHERE owner='$username'");
+			while($row = mysqli_fetch_array($query)) {
+				$id = $row['id'];
+				$name = $row['name'];
+				$dropdown = $dropdown . "<option value='$id'>$name</option>";
+			}
+			return $dropdown . "</select>";
+		}
 	}
 ?>
