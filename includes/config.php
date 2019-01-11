4d979c3460400916cc1b1a433097e34fcdbea085
pcrubianoa <pcrubianoa@gmail.com>
2019-01-11 11:46:45 -0500

    Edit config

--- includes/config.php
+++ includes/config.php
@@ -6,7 +6,7 @@
     $timezone = date_default_timezone_set("America/Bogota");
 
     //$con = mysqli_connect("localhost", "root", "", "spotifyclone");
-    $con = mysqli_connect("ec2-107-22-238-186.compute-1.amazonaws.com", "njyuewanyiurpq", "455e204572c677b7e761cb0a5f338d20e295d7068cb134064f10f06105025b46", "dcfvmkl99lqlnd");
+    $con = mysqli_connect("ec2-107-22-238-186.compute-1.amazonaws.com", "njyuewanyiurpq", "455e204572c677b7e761cb0a5f338d20e295d7068cb134064f10f06105025b46", "dcfvmkl99lqlnd", "5432");
 
     if(mysqli_connect_errno()){
         echo "Failed to connect: " . mysqli_connect_errno();
